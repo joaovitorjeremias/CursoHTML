7ff0483443711be33d21d37ce62a9cd02bd46734
Voao-Jitor <joaovitorjeremias23@gmail.com>
2025-06-18 23:12:49 -0300

    form

---
+++ formulario/script.js
@@ -0,0 +1,9 @@
+function foco_label(){
+  document.getElementById("label-email").style.color = "white"
+  document.getElementById("label-senha").style.color = "white"
+}
+
+function blur_label(){
+  document.getElementById("label-email").style.color = "transparent"
+  document.getElementById("label-senha").style.color = "transparent"
+}
